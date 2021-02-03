95a34b140887589fe9ed02e2068dc73c2cecf46c
Hugot99 <hugothelosen99@gmail.com>
2021-02-03 16:57:29 +0100

    --all

---
+++ Test.py
@@ -0,0 +1,65 @@
+import tensorflow as tf
+
+# name = input("Enter your name:")
+# age = input("Enter your age:")
+# num1 = input("Enter number:")
+# num2 = input("Enter another number:")
+# print("Hello " + name + ",you are: "+ age)
+# result = int(num1) + int(num2)
+# print(result)
+## function ##
+# def say_hi(name):
+#     print('hello' + name)
+#
+# say_hi('hugo')
+
+# #
+#
+# def cube(num):
+#     return num*num*num
+#
+#
+# print(cube(9))
+#
+# is_male = True
+# is_tall = False
+#
+# if is_male and not(is_tall):
+#     print('Male or tall or both')
+# else:
+#     print('Women')
+#
+# num1 = float(input("first number"))
+# op = input("enter operator: ")
+# num2 = float(input("second number"))
+#
+# if op == "+":
+#     print(num1+num2)
+# elif op == "-":
+#     print(num1-num2)
+# elif op == "*":
+#     print(num1*num2)
+# else:
+#     print(num1/num2)
+# #
+# a = [1,2,4,5,6,67]
+# for index in range(len(a)):
+#     print(a[index])
+#
+number_grid = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9],
+    [0]
+]
+
+for row  in number_grid:
+    for col in row:
+        print(col)
+
+
+
+for i in range(1,10):
+    i +=1
+    print(i)
+
